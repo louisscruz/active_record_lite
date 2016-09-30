aff9f5dcbfe9c1f965c79004c3bc9106b236d5e7
louisscruz <louisstephancruz@me.com>
2016-09-30 16:04:11 -0700

    Finish 4

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -51,41 +51,27 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
     define_method(name) do
-      koptions = BelongsToOptions.new(name, options)
+      koptions = self.class.assoc_options[name]
       key_value = self.send(koptions.primary_key)
       koptions.model_class.where(koptions.primary_key => key_value).first
     end
   end
 
   def has_many(name, options = {})
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
     define_method(name) do
-      koptions = HasManyOptions.new(name, self.class.name, options)
+      koptions = self.class.assoc_options[name]
       key_value = self.send(koptions.primary_key)
       koptions.model_class.where(koptions.foreign_key => key_value)
     end
   end
 
-  # def has_many(name, options = {})
-  #   p self
-  #   self.assoc_options[name] =
-  #     HasManyOptions.new(name, self.name, options)
-  #
-  #   define_method(name) do
-  #     options = self.class.assoc_options[name]
-  #
-  #     key_val = self.send(options.primary_key)
-  #     options
-  #       .model_class
-  #       .where(options.foreign_key => key_val)
-  #   end
-  # end
-  #
-  # def assoc_options
-  #   @assoc_options ||= {}
-  #   @assoc_options
-  #   # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
-  # end
+  def assoc_options
+    @assoc_options ||= {}
+    @assoc_options
+  end
 end
 
 class SQLObject
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -2,9 +2,35 @@ require_relative '03_associatable'
 
 # Phase IV
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
+  # Remember to go back to 03_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      through_primary_key = through_options.primary_key
+      through_foreign_key = through_options.foreign_key
+
+      source_table = source_options.table_name
+      source_foreign_key = source_options.foreign_key
+
+      key_val = self.send(through_foreign_key)
+
+      results = DBConnection.execute(<<-SQL, key_val)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+        ON
+          #{through_table}.#{source_foreign_key} = #{source_table}.#{through_primary_key}
+        WHERE
+          #{through_table}.#{through_primary_key} = ?
+      SQL
+      source_options.model_class.parse_all(results).first
+    end
   end
 end
